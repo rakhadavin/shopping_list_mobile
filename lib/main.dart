70608d07629e421078c3795e7091425d3d62c37e
rakhadavin <rakha.davin@ui.ac.id>
2023-11-22 15:47:48 +0700

    add workflows

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:shopping_list/screen/login.dart';
-import 'package:shopping_list/screen/menu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +24,7 @@ class MyApp extends StatelessWidget {
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.amberAccent),
             useMaterial3: true,
           ),
-          home: LoginPage(),
+          home: const LoginPage(),
         ));
   }
 }
